e449dc4674b47291f6c14a475bcbc8f04eadeda5
Belem Zhang <belem.zhang@intel.com>
2022-05-06 08:25:10 +0800

    adding dist

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': '@vue/vue3-jest',
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': 'ts-jest',
   },
   // Match .js/.ts or xx.test.js/ts xx.spec.js/ts under __tests__
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1' // config jest @ -> src
-  }
-}
+    '^@/(.*)$': '<rootDir>/src/$1', // config jest @ -> src
+  },
+};
